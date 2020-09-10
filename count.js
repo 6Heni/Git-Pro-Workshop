2255661d3a40fb229d6cf5ab720b72c3e2c0179f
tamasgrg <gorog@consolidity.com>
2020-09-10 20:04:09 +0200

    a4

--- count.js
+++ count.js
@@ -1,4 +1,4 @@
-function count(age, city) {
+function count(age, city, month) {
     console.log(`The month of your birth is: ${month}`);
     console.log(`Your age is: ${age}`);
     console.log(`You live in: ${city}`);

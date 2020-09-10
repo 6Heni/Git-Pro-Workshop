359ebbd53147cd58e270e1a09b9a5b85f56aa70a
6Heni <henrietta.hatos@gmail.com>
2020-09-10 20:07:20 +0200

    b5

--- count.js
+++ count.js
@@ -1,5 +1,6 @@
-function count(age, city) {
+function count(age, city, job) {
     console.log(`The month of your birth is: ${month}`);
     console.log(`Your age is: ${age}`);
     console.log(`You live in: ${city}`);
+    console.log(`You work as: ${job}`);
 }
